aca502ebc1001da86c2bce859886f2658d14623e
Richard Sartor <richard.sartor@rackspace.com>
2013-06-12 15:01:47 -0500

    Filled in DeproxyShutdownTest.

--- src/test/groovy/org/rackspace/gdeproxy/DeproxyShutdownTest.groovy
+++ src/test/groovy/org/rackspace/gdeproxy/DeproxyShutdownTest.groovy
@@ -5,12 +5,54 @@
 
 package org.rackspace.gdeproxy
 
+import org.junit.*;
+import static org.junit.Assert.*;
+
 /**
  *
  * @author richard-sartor
  */
 class DeproxyShutdownTest {
-	
+  
+  @Test
+  def testShutdown(){
+    
+    /*
+     *  When a Deproxy shuts down, all of its endpoints are shut down and 
+     *  removed, which means that the ports they were using should be available 
+     *  again.
+     *  
+     */
+    
+    def pf = new PortFinder();
+    def port1 = pf.getNextOpenPort();
+    def port2 = pf.getNextOpenPort();
+      
+    def deproxy = new Deproxy();
+    
+    def e1 = deproxy.addEndpoint(port1)
+    def e2 = deproxy.addEndpoint(port2)
+    
+    deproxy.shutdown()
+    
+    try {
+
+      def e3 = deproxy.addEndpoint(port1)
+      Assert.assertNotNull(e3)
+
+    } catch (IOException e) {
+      Assert.fail("addEndpoint threw an exception")
+    }
+    
+    try {
+
+      def e4 = deproxy.addEndpoint(port2)
+      Assert.assertNotNull(e4)
+
+    } catch (IOException e) {
+      Assert.fail("addEndpoint threw an exception")
+    }
+  }
 }
 
 
